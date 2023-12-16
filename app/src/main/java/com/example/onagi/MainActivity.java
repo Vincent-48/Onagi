e77c2d1f7bd78f560b465633c9d3fbabed72df3a
Vince <vincentochieng33@gmail.com>
2023-12-17 00:28:58 +0300

    initial commit

--- app/src/main/java/com/example/onagi/MainActivity.java
+++ app/src/main/java/com/example/onagi/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.onagi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -10,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView txtName;
     private TextView txtAbout;
     private ImageView txtProfilePic;
-    
+    Button button1, button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +24,26 @@ public class MainActivity extends AppCompatActivity {
         txtName = findViewById(R.id.txtName);
         txtAbout = findViewById(R.id.txtAbout);
         txtProfilePic = findViewById(R.id.imageView);
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                replaceFragment(new Fragment1());
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                replaceFragment(new Fragment2());
+            }
+        });
+    }
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout,fragment);
+        fragmentTransaction.commit();
     }
 }
