3e729b154a7feccd5c5742850b53d0997921c468
Vince <vincentochieng33@gmail.com>
2023-12-18 23:41:34 +0300

    initial commit

--- app/src/main/java/com/example/onagi/MessageFragment.java
+++ app/src/main/java/com/example/onagi/MessageFragment.java
@@ -28,14 +28,7 @@ public class MessageFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MessageFragment.
-     */
+
     // TODO: Rename and change types and number of parameters
     public static MessageFragment newInstance(String param1, String param2) {
         MessageFragment fragment = new MessageFragment();
