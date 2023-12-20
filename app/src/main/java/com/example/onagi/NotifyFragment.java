dfaafcafc2e7d1e136cd811c87540a929e2814c8
Vince <vincentochieng33@gmail.com>
2023-12-20 23:33:44 +0300

    initial commit

--- app/src/main/java/com/example/onagi/NotifyFragment.java
+++ app/src/main/java/com/example/onagi/NotifyFragment.java
@@ -24,14 +24,7 @@ public class NotifyFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment NotifyFragment.
-     */
+
     // TODO: Rename and change types and number of parameters
     public static NotifyFragment newInstance(String param1, String param2) {
         NotifyFragment fragment = new NotifyFragment();
