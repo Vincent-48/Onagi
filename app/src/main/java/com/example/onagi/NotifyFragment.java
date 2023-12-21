af27a857f8d75210dab8e558b55a7bdf142c72b8
Vince <vincentochieng33@gmail.com>
2023-12-21 23:42:50 +0300

    initial commit

--- app/src/main/java/com/example/onagi/NotifyFragment.java
+++ app/src/main/java/com/example/onagi/NotifyFragment.java
@@ -13,20 +13,7 @@ public class NotifyFragment extends Fragment {
 
 
 
-    public NotifyFragment() {
-        // Required empty public constructor
-    }
 
-
-    // TODO: Rename and change types and number of parameters
-    public static NotifyFragment newInstance(String param1, String param2) {
-        NotifyFragment fragment = new NotifyFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
 
 
