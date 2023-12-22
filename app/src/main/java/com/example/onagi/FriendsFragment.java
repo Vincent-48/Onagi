b8f06310886e7aa5f712e25b9ab7ebdadc611c91
Vince <vincentochieng33@gmail.com>
2023-12-22 23:40:15 +0300

    initial commit

--- app/src/main/java/com/example/onagi/FriendsFragment.java
+++ app/src/main/java/com/example/onagi/FriendsFragment.java
@@ -31,14 +31,7 @@ public class FriendsFragment extends Fragment {
         return fragment;
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
