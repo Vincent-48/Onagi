68e13cf8c2522072de4fc29a6d0155b072c9c5c8
Vince <vincentochieng33@gmail.com>
2023-12-23 23:47:41 +0300

    initial commit

--- app/src/main/java/com/example/onagi/ProfileFragment.java
+++ app/src/main/java/com/example/onagi/ProfileFragment.java
@@ -10,6 +10,7 @@ import android.view.ViewGroup;
 
 
 public class ProfileFragment extends Fragment {
+    View view;
 
 
 
@@ -21,6 +22,7 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        view = inflater.inflate(R.layout.fragment_profile, container, false);
+        return view;
     }
 }
