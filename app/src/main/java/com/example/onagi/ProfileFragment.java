9b162c577db40fa9fd4961ef5413ea27d4793d27
Vince <vincentochieng33@gmail.com>
2023-12-23 23:45:42 +0300

    initial commit

--- app/src/main/java/com/example/onagi/ProfileFragment.java
+++ app/src/main/java/com/example/onagi/ProfileFragment.java
@@ -11,18 +11,7 @@ import android.view.ViewGroup;
 
 public class ProfileFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ProfileFragment() {
-        // Required empty public constructor
-    }
 
     /**
      * Use this factory method to create a new instance of
