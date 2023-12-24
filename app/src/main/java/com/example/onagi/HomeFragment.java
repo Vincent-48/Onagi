cc3f310bb42d5d701b7a6579cf83a55d15c332dc
Vince <vincentochieng33@gmail.com>
2023-12-24 23:50:01 +0300

    initial commit

--- app/src/main/java/com/example/onagi/HomeFragment.java
+++ app/src/main/java/com/example/onagi/HomeFragment.java
@@ -8,21 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/
+
 public class HomeFragment extends Fragment {
 
 
 
 
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
