d2078915300e4c7d6b051ff35d904276546b05f9
Vince <vincentochieng33@gmail.com>
2023-12-24 23:51:00 +0300

    initial commit

--- app/src/main/java/com/example/onagi/HomeFragment.java
+++ app/src/main/java/com/example/onagi/HomeFragment.java
@@ -10,6 +10,7 @@ import android.view.ViewGroup;
 
 
 public class HomeFragment extends Fragment {
+    View view;
 
 
 
@@ -21,6 +22,7 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        view = inflater.inflate(R.layout.fragment_home, container, false);
+        return view;
     }
 }
