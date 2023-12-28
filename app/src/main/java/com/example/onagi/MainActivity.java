2d6317a51a6ac9b6bb48e0aeda52c246ca1dc337
Vince <vincentochieng33@gmail.com>
2023-12-28 16:26:14 +0300

    initial commit

--- app/src/main/java/com/example/onagi/MainActivity.java
+++ app/src/main/java/com/example/onagi/MainActivity.java
@@ -25,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
     }
     private void replaceFragment(Fragment fragment){
         FragmentManager fragmentManager = getSupportFragmentManager();
